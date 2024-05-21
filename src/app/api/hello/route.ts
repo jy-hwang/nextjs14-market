b93026a7930dbeeb1c98220dbf0cfa14a59b47ac
jy-hwang <stratojy@gmail.com>
2024-05-22 01:15:09 +0900

    권한에 따른 인가처리
    로그인 한 사람만 특정경로에 접근할 수 있도록 권한에 따른 인가처리를 추가하였음.
    
    nextjs 에서 HTTP Method 를 사용하는 방법에 대해 간단히 연습해봄.

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,9 @@
+export async function GET(request : Request){
+    return new Response('안녕하세요, Hello, GET');
+}
+
+export async function POST(request : Request){
+    return new Response('안녕하세요, Hello, POST');
+}
+    
+    
