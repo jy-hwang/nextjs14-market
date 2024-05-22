135c875cba5606ada6f044e118f815009c98bd87
jy-hwang <stratojy@gmail.com>
2024-05-22 16:54:54 +0900

    미들웨어에서 정적파일을 제외처리하는 2가지 방법
    로그아웃 상태에서도 middleware session 콘솔로그가 여러번 찍혀서 뭔가해서 pathname 을 같이 봤는데
    'middleware pathname :  /_next/static/css/app/layout.css ,  session :  null' 이런식으로 찍혀서
    정적 파일들을 제외시켜주는 방법을 찾아봤고 1. 정규식을 사용한 방법 2. 정규식을 사용하지 않는 방법 이 있었고,
    일단 2가지 방법을 모두 소스코드에 포함시켜놓기로 함. 정규식을 사용한 방법을 채택하였음.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,27 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export { default } from 'next-auth/middleware';
 
-// export const config = { matcher: ['/admin/:path*', '/user'] };
+export const config = {
+  matcher: [
+    //'/admin/:path*',
+    //'/user',
+    //'/((?!api|_next/static|_next/image|favicon.ico).*)',
+    // 모든 API 경로 , 정적파일경로(_next/static), 이미지 경로(_next/image) , favicon.ico 제외
+  ]
+};
 
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: process.env.JWT_SECRET });
   const pathname = req.nextUrl.pathname;
 
-  console.log("middleware session : ", session);
+  console.log("middleware pathname : ", pathname, ",  session : ", session);
+
+  // 정규식을 이용한 정적파일(css, js 등)과 이미지, favicon.ico 등을 미들웨어 제외처리
+  const staticFilePattern = /\.(.*)$/;//경로의 끝이 파일 확장자로 끝나는지를 확인
+
+  if (staticFilePattern.test(pathname)) {
+    return NextResponse.next();
+  }
 
   if (req.nextUrl.pathname.startsWith('/user') && !session) {
     return NextResponse.redirect(new URL('/auth/login', req.url));
