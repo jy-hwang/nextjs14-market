7a916eb4f07c5fc05b40474f3c5b7ddb82b18fbf
jy-hwang <stratojy@gmail.com>
2024-05-22 15:04:58 +0900

    색상 변경
    오랜지 - 로즈 색 보다는 눈이 편안한 파란색이 좋다

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='relative z-10 w-full text-white bg-orange-500'>
+    <nav className='relative z-10 w-full text-white bg-blue-500'>
       <div className='flex items-center justify-between mx-5 sm:mx-10 lg:mx-20'>
         {/* logo */}
         <div className='flex items-center text-2xl h-14'>
