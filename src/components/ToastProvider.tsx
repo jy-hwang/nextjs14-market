28573f24fc837a423ae9221f33167cec063313ad
jy-hwang <stratojy@gmail.com>
2024-05-24 17:31:57 +0900

    toast 관련 파일 404 에러
    1. min.css 를 가져오려는데 에러가 .map 파일을 못찾는다는 오류메시지가 떴음.
    2. https://github.com/fkhadra/react-toastify/issues/1099 에 나랑 똑같은 오류가 발생하는 사람이 많음.
    3. 서버실행이나, 실제 toast 메시지가 뜨는 데에는 문제가 없는데, 단순히 서버로그에 404가 뜨니깐 조금 찝찝한 것일 뿐.
    4. 일단 다른 사람들이 얘기한 대로, /dist 폴더를 지우고 .css 로 가져오는 것으로 일단락.  차후에 이슈해결이 되었으면 좋겠다

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-toastify/ReactToastify.css'
 
 const ToastProvider = () => {
   return (
