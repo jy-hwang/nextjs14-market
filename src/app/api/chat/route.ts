fd2c4675123ef83dd46f71fc53421080c378b9d5
jy-hwang <stratojy@gmail.com>
2024-05-27 00:48:27 +0900

    채팅 메세지를 위한 Route 생성
    1. Conversation 테이블을 조회하여 둘이 대화를 나눴던 기록이 있다면
    1-1. Message 테이블에만 대화를 기록함.
    2. 기록이 없다면
    2-1. Conversation 테이블에 create
    2-2. Message 테이블에 create

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -29,6 +29,94 @@ const currentUser = await getCurrentUser();
             }
         }
     })
-    
+
     return NextResponse.json(users);
 }
+
+
+export async function POST(
+    request: Request
+){
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return NextResponse.error();
+    }
+
+    const body = await request.json();
+
+    // 이미 둘이 대화를 한 conversation 이 있는지 찾기
+    const conversation = await prisma.conversation.findFirst({
+        where: {
+            AND: [
+                {
+                    users: {
+                        some: {
+                            id: body.senderId
+                        }
+                    }
+                },
+                {
+                    users: {
+                        some: {
+                            id: body.receiverId
+                        }
+                    }
+                },
+            ]
+        }
+    });
+
+    if(conversation){
+        // 이미 둘이 대화를 한 conversation 이 있다면 메시지만 생성하기
+        try {
+            const message = await prisma.message.create({
+                data: {
+                    text: body.text,
+                    image: body.image,
+                    senderId: body.senderId,
+                    receiverId: body.receiverId,
+                    conversationId: conversation.id,
+                }
+            })
+            return NextResponse.json(message);
+        } catch (error) {
+            return NextResponse.json(error);
+        }
+    } else {
+        //둘이 처음 대화하는 거라면 conversation 과 message 둘 다 생성
+        // conversation 생성시 존재하는 유저 record 와 연결
+        const newConversation = await prisma.conversation.create({
+            data: {
+                senderId: body.senderId,
+                receiverId: body.receiverId,
+                users: {
+                    connect: [
+                        {
+                            id: body.senderId,
+                        },
+                        {
+                            id: body.receiverId,
+                        }
+                    ]
+                }
+            }
+        });
+
+        try {
+            const message = await prisma.message.create({
+                data: {
+                    text: body.text,
+                    image: body.image,
+                    conversationId: newConversation.id,
+                    senderId: body.senderId,
+                    receiverId: body.receiverId,
+                }
+            })
+            return NextResponse.json(message);
+        } catch (error) {
+            return NextResponse.json(error);
+        }
+    }
+}
+
