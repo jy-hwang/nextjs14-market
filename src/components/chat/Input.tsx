92aaa6d28b67960df7c0b8a4f4d6adb975ca9368
jy-hwang <stratojy@gmail.com>
2024-05-27 18:49:09 +0900

    useSWRMutation 이용
    1. 적용하기 전과 후를 비교해서 어떤 점이 나은지, 불편한지 등을 한번 체크해보면 좋을 것 같다.

--- src/components/chat/Input.tsx
+++ src/components/chat/Input.tsx
@@ -2,63 +2,80 @@ import axios from 'axios';
 import React, { FormEvent, useState } from 'react'
 import { IoImageOutline } from 'react-icons/io5';
 import { RiSendPlaneLine } from 'react-icons/ri';
+import useSWRMutation from 'swr/mutation';
 
-interface InputProps{
+interface InputProps {
     receiverId: string;
     currentUserId: string;
 }
 
+const sendRequest = (url: string, { arg }: {
+    arg: {
+        text: string;
+        image: string;
+        receiverId: string;
+        senderId: string;
+    }
+}) => {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(arg)
+    }).then(res => res.json());
+}
+
 const Input = ({
     receiverId, currentUserId
-}:InputProps) => {
+}: InputProps) => {
 
     const [message, setMessage] = useState('');
+
+    const { trigger } = useSWRMutation('/api/chat', sendRequest)
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-    const imageUrl = '';
+        const imageUrl = '';
 
-    if( message || imageUrl){
-        try {
-            await axios.post('/api/chat',{
-                text: message,
-                image: imageUrl,
-                receiverId: receiverId,
-                senderId: currentUserId
-            });
-        } catch (error) {
-            console.error(error);
+        if (message || imageUrl) {
+            try {
+                trigger({
+                    text: message,
+                    image: imageUrl,
+                    receiverId: receiverId,
+                    senderId: currentUserId
+                });
+            } catch (error) {
+                console.error(error);
+            }
         }
+
+        setMessage('');
+
     }
 
-    setMessage('');
-    
-    }
-
-  return (
-    <form
-        onSubmit={handleSubmit}
-        className='relative flex items-center justify-between w-full gap-4 p-2 pl-4 border-[1px] border-gray-300 rounded-md shadow-sm'
-    >
-        <input
-            className='w-full text-base outline-none'
-            type="text"
-            placeholder='메시지를 작성해주세요'    
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-        />
-        <div className='text-2xl text-gray-200 cursor-pointer'>
-            <IoImageOutline />
-        </div>
-        <button
-            type='submit'
-            className='flex items-center justify-center p-2 text-gray-900 bg-blue-500 rounded-lg cursor-pointer hover:bg-blue-600 disabled:opacity-60'
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className='relative flex items-center justify-between w-full gap-4 p-2 pl-4 border-[1px] border-gray-300 rounded-md shadow-sm'
         >
-            <RiSendPlaneLine className='text-white'/>
-        </button>
-    </form>
-  )
+            <input
+                className='w-full text-base outline-none'
+                type="text"
+                placeholder='메시지를 작성해주세요'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <div className='text-2xl text-gray-200 cursor-pointer'>
+                <IoImageOutline />
+            </div>
+            <button
+                type='submit'
+                className='flex items-center justify-center p-2 text-gray-900 bg-blue-500 rounded-lg cursor-pointer hover:bg-blue-600 disabled:opacity-60'
+            >
+                <RiSendPlaneLine className='text-white' />
+            </button>
+        </form>
+    )
 }
 
 export default Input
