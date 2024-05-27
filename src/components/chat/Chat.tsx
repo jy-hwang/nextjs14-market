609cece3d25caa7144e1875d732ee431441ff38e
jy-hwang <stratojy@gmail.com>
2024-05-27 09:34:29 +0900

    ChatHeader Component 생성
    1. lastMessageTime
    1-1. 서로 나눈 대화가 있는 경우 상대방이 보낸 마지막 메시지의 보낸 시각을 보여줌.
    
    2. 간단한 line formatting

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,15 +1,17 @@
-import { TUserWithChat } from '@/types'
+import { TConversation, TUserWithChat } from '@/types'
 import React from 'react'
 import Input from './Input';
+import ChatHeader from './ChatHeader';
+import { User } from '@prisma/client';
 
-interface ChatProps{
+interface ChatProps {
     currentUser: TUserWithChat;
     receiver: {
         receiverId: string,
         receiverName: string,
-        receiverImage: string,        
+        receiverImage: string,
     },
-    setLayout:(layout:boolean) => void;
+    setLayout: (layout: boolean) => void;
 }
 
 const Chat = ({
@@ -18,22 +20,36 @@ const Chat = ({
     setLayout
 }: ChatProps) => {
 
-    if(!receiver.receiverName || !currentUser){
+    if (!receiver.receiverName || !currentUser) {
         return <div className='w-full h-full'></div>
     }
+
+    const conversation =
+        currentUser?.conversations.find((conversation: TConversation) =>
+            conversation.users.find((user: User) => user.id === receiver.receiverId)
+        );
+
+    const lastMessageTime = conversation?.messages
+        .filter((message) => message.receiverId === currentUser.id)
+        .slice(-1)[0]?.createdAt;
 
     return (
         <div className='w-full'>
             <div>
-                {/* chat header */}
+                <ChatHeader
+                    setLayout={setLayout}
+                    receiverName={receiver.receiverName}
+                    receiverImage={receiver.receiverImage}
+                    lastMessageTime={lastMessageTime}
+                />
             </div>
             <div className='flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]'>
                 {/* chat message */}
             </div>
             <div>
                 <Input
-                receiverId={receiver?.receiverId}
-                currentUserId={currentUser?.id}
+                    receiverId={receiver?.receiverId}
+                    currentUserId={currentUser?.id}
                 />
             </div>
         </div>
